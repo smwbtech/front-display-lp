63f23f52fcee4a1a4282e8ec5d03f2d7460c8893
smwbtech <smwb.tech@gmail.com>
2019-07-24 11:50:19 +0300

    Update FeaturesBlock.vue
    * Добавлены переходы для иллюстраций и текста
    * Мониторинг скролла и появления блока во вьюпорте
    * Добавлен метод автопереключения слайдов

--- components/blocks/FeaturesBlock.vue
+++ components/blocks/FeaturesBlock.vue
@@ -1,36 +1,44 @@
 <template lang="html">
 	<section class="features-block">
+		<!-- Блок иллюстраций -->
 		<div class="illustration">
-			<div class="image">
-				<img
-					v-for="illustration in currentFeature.illustrations"
-					:key="illustration"
-					:src="illustration"
-					class="image__item"
-					alt=""
-				/>
-			</div>
+			<transition name="image-appear" mode="out-in">
+				<div v-if="!isChanging" class="image">
+					<img
+						v-for="illustration in currentFeature.illustrations"
+						:key="illustration"
+						:src="illustration"
+						class="image__item"
+						alt=""
+					/>
+				</div>
+			</transition>
 		</div>
 
+		<!-- Текстовый блок -->
 		<div class="text">
 			<RombLine class="text__decore-line" />
 			<h2 class="title">
 				характеристики промышленного монитора DNA
 			</h2>
 			<div class="feature-text">
-				<h3 class="feature-text__title">
-					{{ currentFeature.title }}
-				</h3>
-				<p class="feature-text__text">
-					{{ currentFeature.description }}
-				</p>
+				<transition name="title-appear">
+					<h3 v-if="!isChanging" class="feature-text__title">
+						{{ currentFeature.title }}
+					</h3>
+				</transition>
+				<transition name="text-appear" mode="out-in">
+					<p v-if="!isChanging" class="feature-text__text">
+						{{ currentFeature.description }}
+					</p>
+				</transition>
 			</div>
 			<ul class="nav">
 				<li
 					v-for="(component, index) in navComponents"
 					:key="`${index}-nav`"
 					class="nav-item"
-					@click="changeSlide(index)"
+					@click="changeByClick(index)"
 				>
 					<component
 						:is="`${component}`"
@@ -46,7 +54,10 @@
 export default {
 	data() {
 		return {
+			isObserved: false,
+			isChanging: false,
 			current: 0,
+			timerId: undefined,
 			navComponents: [
 				'Ip65Nav',
 				'TemperatureNav',
@@ -87,9 +98,9 @@ export default {
 				},
 				{
 					illustrations: [
-						'img/features-display/customizatiom_illustration_left.jpg',
-						'img/features-display/customizatiom_illustration_center.jpg',
-						'img/features-display/customizatiom_illustration_right.jpg'
+						'img/features-display/touch_illustration_left.jpg',
+						'img/features-display/touch_illustration_center.jpg',
+						'img/features-display/touch_illustration_right.jpg'
 					],
 					title: 'Широкий ассортимент модификаций и опций',
 					description:
@@ -97,9 +108,9 @@ export default {
 				},
 				{
 					illustrations: [
-						'img/features-display/touch_illustration_left.jpg',
-						'img/features-display/touch_illustration_center.jpg',
-						'img/features-display/touch_illustration_right.jpg'
+						'img/features-display/customization_illustration_left.jpg',
+						'img/features-display/customization_illustration_center.jpg',
+						'img/features-display/customization_illustration_right.jpg'
 					],
 					title: 'Сенсорный экран',
 					description:
@@ -109,7 +120,15 @@ export default {
 		}
 	},
 
+	mounted() {
+		this.observeScroll(this.$el)
+	},
+
 	computed: {
+		/**
+		 * Возвращает текущее свойство
+		 * @return {object} - возвращает объект текущего свойства товара
+		 */
 		currentFeature() {
 			return this.features[this.current]
 		}
@@ -117,12 +136,65 @@ export default {
 
 	methods: {
 		/**
+		 * Отслеживаем, когда последний элемент
+		 *
+		 * @param  {Object} el - последний элемент текущего списка
+		 * @return {undefined}
+		 */
+		observeScroll(el) {
+			const options = {
+				threshold: 0.7
+			}
+
+			const callback = (entries, observer) => {
+				for (const entry of entries) {
+					const { isIntersecting } = entry
+					if (isIntersecting) {
+						this.isObserved = true
+						this.autoSlider()
+						observer.disconnect()
+					}
+				}
+			}
+			const observer = new IntersectionObserver(callback, options)
+			observer.observe(el)
+		},
+		/**
 		 * Меняем слайд
 		 * @param  {number} i 	 - номер слайда
 		 * @return {undefined}   [description]
 		 */
 		changeSlide(i) {
-			this.current = i
+			if (i !== this.current) {
+				this.isChanging = true
+				this.current = i
+				setTimeout(() => (this.isChanging = false), 300)
+			}
+		},
+		/**
+		 * Автоматическая смена слайдов
+		 * @return {[type]} [description]
+		 */
+		autoSlider() {
+			this.timerId = setInterval(() => {
+				const nextSlide =
+					this.current < this.features.length - 1
+						? this.current + 1
+						: 0
+				this.changeSlide(nextSlide)
+			}, 8000)
+		},
+
+		/**
+		 * Меняем слайд по клику
+		 * и замедляем действие autoSlider
+		 * @param  {number} i - номер слайда
+		 * @return {undefined}
+		 */
+		changeByClick(i) {
+			clearInterval(this.timerId)
+			this.changeSlide(i)
+			setTimeout(this.autoSlider, 8000)
 		}
 	}
 }
@@ -139,16 +211,34 @@ export default {
 
     /* Изображение */
     & .illustration {
+		margin-top: var(--row);
         width: calc(var(--column) * 13 + var(--gutter) * 12);
 
         & .image {
             display: flex;
             justify-content: flex-start;
             width: calc(var(--column) * 12 + var(--gutter) * 11);
-
+			height: 80%;
+			/* Изображения */
             & .image__item {
                 display: block;
                 align-self: center;
+				max-height: calc(var(--row) * 5 + var(--gutter) * 5);
+				transition: transform .3s ease-out .15s;
+
+				&:first-of-type {
+					width: calc(var(--column) + var(--gutter));
+					transform: translateY(40px);
+				}
+
+				&:nth-of-type(2) {
+					width: calc(var(--column) * 8 + var(--gutter) * 7);
+				}
+
+				&:last-of-type {
+					width: calc(var(--column) * 2 + var(--gutter) * 3);
+					transform: translateY(-40px);
+				}
             }
 
         }
@@ -177,8 +267,22 @@ export default {
 			min-height: calc(var(--row) * 3 + var(--gutter) * 2);
 
             & .feature-text__title {
+				position: relative;
                 font-family: 'Orpheus', serif;
                 font-size: 2em;
+				margin-bottom: 40px;
+
+				&:after {
+					content: "";
+					display: block;
+					position: absolute;
+					z-index: 20;
+					bottom: -40px;
+					left: 0;
+					width: calc(var(--column) * 3 + var(--gutter) * 2);
+					height: 20px;
+					background-color: #000;
+				}
             }
 
             & .feature-text__text {
@@ -205,6 +309,8 @@ export default {
 				width: 30px;
 				height: 30px;
 				cursor: pointer;
+				fill: #000;
+				transition: fill .3s ease-in;
 
 				& .active {
 					fill: var(--blue);
@@ -212,6 +318,67 @@ export default {
 			}
 		}
     }
+}
 
+/* Анимации и переходы */
+
+/* Изображение */
+.image-appear-enter {
+	transform: translateX(-40px);
+	opacity: 0;
+
+	& .image__item {
+
+		&:first-of-type,
+		&:last-of-type {
+			transform: translateY(0) !important;
+		}
+	}
+}
+
+.image-appear-leave-to {
+	& .image__item {
+
+		&:first-of-type {
+			transform: translateY(60px) !important;
+		}
+
+		&:last-of-type {
+			transform: translateY(-60px) !important;
+		}
+	}
+}
+
+.image-appear-leave-to {
+	transform: translateX(40px);
+	opacity: 0;
+}
+
+.image-appear-enter-active,
+.image-appear-leave-active {
+	transition: opacity .3s ease-in, transform .35s ease-in;
+}
+
+/* Заголовок */
+
+.title-appear-enter,
+.title-appear-leave-to {
+	opacity: 0;
+	transform: translateY(-20px);
+}
+
+.title-appear-enter-active,
+.title-appear-leave-active {
+	transition: all .3s ease-in;
+}
+.text-appear-enter,
+.text-appear-leave-to {
+	opacity: 0;
+	transform: translateY(20px);
+}
+
+.text-appear-enter-active,
+.text-appear-leave-active {
+	transition: all .3s ease-in;
 }
 </style>
