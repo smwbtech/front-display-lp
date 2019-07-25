10a785c9781a46af294c70c11f037cb9f0832bc6
smwbtech <smwb.tech@gmail.com>
2019-07-25 12:41:47 +0300

    Update index.vue
    Добавлен блок моделей

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,8 @@
 		</PageBlock>
 		<!-- Слайдер с особенностями -->
 		<PageBlock><FeaturesBlock /></PageBlock>
+		<!-- Список моделей -->
+		<PageBlock><ModelsBlock :items="items"/></PageBlock>
 	</div>
 </template>
 
@@ -18,13 +20,33 @@ import PageBlock from '@/components/PageBlock.vue'
 import FirstDisplay from '@/components/blocks/FirstDisplay.vue'
 import DisplayBlock from '@/components/blocks/DisplayBlock.vue'
 import FeaturesBlock from '@/components/blocks/FeaturesBlock.vue'
+import ModelsBlock from '@/components/blocks/ModelsBlock.vue'
+
+import { getToken } from '@/assets/js/getToken.js'
+import { getItems } from '@/assets/js/getItems.js'
 
 export default {
 	components: {
 		PageBlock,
 		FirstDisplay,
 		DisplayBlock,
-		FeaturesBlock
+		FeaturesBlock,
+		ModelsBlock
+	},
+
+	data() {
+		return {
+			tokenReceived: false,
+			dataLoaded: false,
+			sectionId: 297677,
+			items: []
+		}
+	},
+
+	async asyncData() {
+		const token = await getToken()
+		const items = await getItems(token)
+		return { items }
 	}
 }
 </script>
